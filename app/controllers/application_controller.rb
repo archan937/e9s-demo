8a325629632b2fb28804c5e056b681ae86daf518
Paul Engel <paulengel@archan937.local>
2010-11-08 22:22:35 +0100

    Being able to switch locales

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :set_locale
+
+private
+
+  def set_locale
+    session[:locale] = params[:locale] if %w(en nl es).include?(params[:locale])
+    I18n.locale = session[:locale]
+  end
+
 end
