2082fbd63d9aaf4a8e2343905871dc23424c78fd
Paul Engel <paulengel@archan937.local>
2010-11-09 22:11:03 +0100

    Migrate the database

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101108210533) do
+ActiveRecord::Schema.define(:version => 20101109011706) do
+
+  create_table "cms_contents", :force => true do |t|
+    t.string   "key"
+    t.text     "value"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "cms_contents", ["key"], :name => "index_cms_contents_on_key"
 
   create_table "comments", :force => true do |t|
     t.string   "name"
@@ -19,5 +28,35 @@ ActiveRecord::Schema.define(:version => 20101108210533) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "translations", :force => true do |t|
+    t.string   "locale"
+    t.string   "key"
+    t.text     "value"
+    t.boolean  "is_proc",    :default => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "translations", ["key"], :name => "index_translations_on_key"
+  add_index "translations", ["locale"], :name => "index_translations_on_locale"
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "crypted_password"
+    t.string   "password_salt"
+    t.datetime "created_at"
+    t.string   "persistence_token",   :default => "", :null => false
+    t.string   "single_access_token", :default => "", :null => false
+    t.string   "perishable_token",    :default => "", :null => false
+    t.integer  "login_count"
+    t.datetime "last_request_at"
+    t.datetime "current_login_at"
+    t.datetime "last_login_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], :name => "index_users_on_email"
 
 end
