3ff41d589b81722ba19cf11ad4c50ee1ada6a8d0
Paul Engel <paulengel@archan937.local>
2010-11-10 02:20:21 +0100

    Using e9s v2.2.2 (woot!)

--- app/models/translation.rb
+++ app/models/translation.rb
@@ -1,7 +1,8 @@
 class Translation < ActiveRecord::Base
 
   def to_rich_cms_response(params)
-    {:value => value, :translations => Hash[*params[:derivative_keys].split(";").uniq.collect{|x| [x, x.t({:as => params[:editable_input_type]})]}.flatten]}
+    options = {:as => params[:editable_input_type], :capitalize => %w(true 1).include?(params[:capitalize])}
+    {:value => value, :translations => Hash[*params[:derivative_keys].split(";").uniq.collect{|x| [x, x.t(options)]}.flatten]}
   end
 
 end
